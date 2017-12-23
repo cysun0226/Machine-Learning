d49a94e3750a5cdfeaba2cef35b581cb0d8cc880
cysun0226 <j6256448@gmail.com>
2017-12-24 04:12:05 +0800

    finish test decision tree

--- hw3/forestfires.py
+++ hw3/forestfires.py
@@ -57,6 +57,7 @@ shuffle(lines)
 train_set = lines[0:math.floor(0.7*len(lines))]
 test_set = lines[math.floor(0.7*len(lines)):len(lines)]
 
+# train set
 for line in train_set:
     d = []
     d.append(int(line[0])) # X
@@ -78,9 +79,27 @@ forestfire['target'] = array(target)
 forestfire['feature_names'] = array(feature_names)
 forestfire['data'] = array(data)
 
-# print result header
+# test set
 test_data = []
-test_area = []
+test_target = []
+for line in test_set:
+    d = []
+    d.append(int(line[0])) # X
+    d.append(int(line[1])) # Y
+    d.append(month_to_int(line[2])) # month
+    d.append(day_to_int(line[3])) # day
+    d.append(float(line[4])) # FFMC
+    d.append(float(line[5])) # DMC
+    d.append(float(line[6])) # DC
+    d.append(float(line[7])) # ISI
+    d.append(float(line[8])) # temp
+    d.append(float(line[9])) # RH
+    d.append(float(line[10])) # wind
+    d.append(float(line[11])) # rain
+    test_data.append([array(d)])
+    test_target.append(float(line[12]))
+
+# print result header
 total = len(test_set)
 correct = 0
 
@@ -91,4 +110,21 @@ print('test_set = %d' % total)
 # build decision tree
 tree_regressor = DecisionTreeRegressor(random_state=0)
 tree_regressor.fit(forestfire['data'], forestfire['target'])
-p = tree_regressor.predict([forestfire['data'][0]])
+# p = tree_regressor.predict([forestfire['data'][0]])
+
+# test
+print('\n\n----- decision tree -----\n')
+acc_sum = 0
+
+for i in range(len(test_set)):
+    aPredict = tree_regressor.predict(test_data[i])
+
+    x=True if 'a'=='a' else False
+    logTest = 0 if test_target[i] == 0 else math.log10(test_target[i])
+    predTest = 0 if aPredict[0] == 0 else math.log10(aPredict[0])
+    acc = abs(logTest - predTest)
+    acc_sum += acc
+    print('test area = %8.2f | Predict area= %8.2f | diff = %f' % (test_target[i],aPredict[0], acc))
+
+tree_acc = acc_sum / total
+print('\ndecision tree predict diff = %f' % tree_acc)
