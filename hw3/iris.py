685ac5c51de74c24e0e83a575cf5a06d9c2a0375
cysun0226 <j6256448@gmail.com>
2017-12-24 01:53:13 +0800

    finish iris.py

--- hw3/iris.py
+++ hw3/iris.py
@@ -4,9 +4,10 @@ import numpy as np
 from numpy import array
 from sklearn import tree
 from random import shuffle
-from sklearn.neighbors import KDTree
-from sklearn.neighbors import NearestNeighbors
+# from sklearn.neighbors import KDTree
+# from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 
 iris_file = str(sys.argv[1])
 # test_file = str(sys.argv[2])
@@ -32,7 +33,7 @@ for line in train_file.readlines():
 # shuffle and pick the training set
 shuffle(lines)
 train_set = lines[0:math.floor(0.7*len(lines))]
-test_set = lines[math.floor(0.7*len(lines))+1:len(lines)]
+test_set = lines[math.floor(0.7*len(lines)):len(lines)]
 
 for line in train_set:
     row = line.strip('\n')
@@ -42,29 +43,27 @@ for line in train_set:
     row = array(row)
     data.append(row)
     target.append(iClass)
-    # if iClass=='Iris-setosa':
-    #     target.append('Iris-setosa')
-    # elif iClass=='Iris-versicolor':
-    #     target.append('Iris-versicolor')
-    # else: # Iris-virginica
-    #     target.append('Iris-virginica')
 
 iris['target_names'] = array(target_names)
 iris['target'] = array(target)
 iris['feature_names'] = array(feature_names)
 iris['data'] = array(data)
 
-# build decision tree
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(iris['data'], iris['target'])
-
-# test
+# print result header
 test_data = []
 test_iClass = []
 total = len(test_set)
 correct = 0
 
+print('\n\n===== Iris data set =====\n')
+print('train_set = %d' % len(train_set))
 print('test_set = %d' % total)
+
+# build decision tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris['data'], iris['target'])
+
+# test
 print('\n\n----- decision tree -----\n')
 
 for line in test_set:
@@ -102,15 +101,20 @@ for i in range(len(test_set)):
 acc = correct / total
 print('\nkNN accuracy = %f' % acc)
 
-# print(iris['target'][0])
-# print(knn.predict_proba([iris['data'][0]]))
-# print(iris['target'][1])
-# print(knn.predict_proba([iris['data'][1]]))
-# nbrs = NearestNeighbors(n_neighbors=5, algorithm="kd_tree").fit(iris['data'])
-# kdt = KDTree(iris['data'], metric='euclidean')
-# ind = kdt.query([iris['data'][0]], k=5, return_distance=False)
-# print(ind)
+# naïve Bayes
+print('\n\n----- naïve Bayes -----\n')
+nb_clf = GaussianNB()
+nb_clf.fit(iris['data'],iris['target'])
 
-#
-# X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-# >>> kdt = KDTree(X, leaf_size=30, metric='euclidean')
+# test
+correct = 0
+for i in range(len(test_set)):
+    iPredict = nb_clf.predict(test_data[i])
+    if test_iClass[i]==iPredict[0]:
+        correct += 1
+        print('[correct] iClass = {0:15}'.format(test_iClass[i]) + ' | Predict = ' + iPredict[0])
+    else:
+        print('[ wrong ] iClass = {0:15}'.format(test_iClass[i]) + ' | Predict = ' + iPredict[0])
+
+acc = correct / total
+print('\nnaïve Bayes accuracy = %f' % acc)
