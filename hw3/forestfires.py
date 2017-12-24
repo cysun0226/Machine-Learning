2ccc88490859c547d3b8cddbf8500e1af92a2b70
cysun0226 <j6256448@gmail.com>
2017-12-24 22:26:53 +0800

    modify log (log for predict result)

--- hw3/forestfires.py
+++ hw3/forestfires.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy import array
 from random import shuffle
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import normalize
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.naive_bayes import GaussianNB
@@ -28,12 +28,12 @@ def month_to_int(month):
         'apr': 3,
         'may': 4,
         'jun': 5,
-        'jul': 5,
-        'aug': 4,
-        'sep': 3,
-        'oct': 2,
-        'nov': 1,
-        'dec': 0,
+        'jul': 6,
+        'aug': 7,
+        'sep': 8,
+        'oct': 9,
+        'nov': 10,
+        'dec': 11,
     }[month]
 
 def day_to_int(day):
@@ -76,8 +76,11 @@ print('\n\n===== Forestfire data set =====\n')
 print('train_set = %d' % math.floor(0.7*len(lines)))
 print('test_set = %d' % (len(lines) - math.floor(0.7*len(lines))))
 
+sys.stderr.write("\nexecute time: ")
 time = int(input("\nexecute time: "))
+sys.stderr.write("\nprint detailed results?(y/n): ")
 print_result = input("print detailed results?(y/n): ")
+sys.stderr.write("\n\n--- process status ---\n\n")
 
 for x in range(time):
     print('\n\n===== test %d =====' % (x+1))
@@ -114,10 +117,11 @@ for x in range(time):
         cd.append(month_to_int(line[2]))
         cd.append(day_to_int(line[3]))
         cat_data.append(cd)
-        if float(line[12]) == 0:
-            target.append(0) # area
-        else:
-            target.append(math.log10(float(line[12]))) # area
+        target.append(float(line[12]))
+        # if float(line[12]) == 0:
+        #     target.append(-10) # area
+        # else:
+        #     target.append(math.log10(float(line[12]))) # area
 
     forestfire['target'] = array(target)
     forestfire['feature_names'] = array(feature_names)
@@ -154,10 +158,11 @@ for x in range(time):
         cd.append(month_to_int(line[2]))
         cd.append(day_to_int(line[3]))
         testNB_cat_data.append(cd)
-        if float(line[12]) == 0:
-            test_target.append(0) # area
-        else:
-            test_target.append(math.log10(float(line[12]))) # area
+        test_target.append(float(line[12]))
+        # if float(line[12]) == 0:
+        #     test_target.append(-10) # area
+        # else:
+        #     test_target.append(math.log10(float(line[12]))) # area
 
 
     total = len(test_set)
@@ -174,10 +179,10 @@ for x in range(time):
 
     for i in range(len(test_set)):
         aPredict = tree_regressor.predict(test_data[i])
-        # logTest = 0 if test_target[i] == 0 else math.log10(test_target[i])
-        # predTest = 0 if aPredict[0] == 0 else math.log10(aPredict[0])
-        logTest = test_target[i]
-        predTest = aPredict[0]
+        logTest = -1 if test_target[i] == 0 else math.log10(test_target[i])
+        predTest = -1 if aPredict[0] == 0 else math.log10(aPredict[0])
+        # logTest = test_target[i]
+        # predTest = aPredict[0]
         acc = abs(logTest - predTest)
         acc_sum += acc
         if print_result=='y':
@@ -207,7 +212,7 @@ for x in range(time):
             knn_train['data'][r][d] = knn_data[d][0][r]
 
     # kNN regressor
-    neiRgr = KNeighborsRegressor(n_neighbors=10)
+    neiRgr = KNeighborsRegressor(n_neighbors=3)
     neiRgr.fit(knn_train['data'], knn_train['target'])
 
     # test
@@ -217,10 +222,10 @@ for x in range(time):
 
     for i in range(len(test_set)):
         aPredict = neiRgr.predict(test_data[i])
-        # logTest = 0 if test_target[i] == 0 else math.log10(test_target[i])
-        # predTest = 0 if aPredict[0] == 0 else math.log10(aPredict[0])
-        logTest = test_target[i]
-        predTest = aPredict[0]
+        logTest = -1 if test_target[i] == 0 else math.log10(test_target[i])
+        predTest = -1 if aPredict[0] == 0 else math.log10(aPredict[0])
+        # logTest = test_target[i]
+        # predTest = aPredict[0]
         acc = abs(logTest - predTest)
         acc_sum += acc
         if print_result=='y':
@@ -247,9 +252,7 @@ for x in range(time):
         mp = mNB.predict([forestfire_nb['cat_data'][i]])
         g = float(gp[0].decode('UTF-8'))
         m = float(mp[0].decode('UTF-8'))
-        p = []
-        p.append(g)
-        p.append(m)
+        p = [g,m]
         hy_pred.append(p)
     hy_pred = array(hy_pred)
 
@@ -270,10 +273,10 @@ for x in range(time):
         hp = hNB.predict([p])
         h = float(hp[0].decode('UTF-8'))
 
-        # logTest = 0 if test_target[i] == 0 else math.log10(test_target[i])
-        # predTest = 0 if h == 0 else math.log10(h)
-        logTest = test_target[i]
-        predTest = h
+        logTest = -1 if test_target[i] == 0 else math.log10(test_target[i])
+        predTest = -1 if h == 0 else math.log10(h)
+        # logTest = test_target[i]
+        # predTest = h
         acc = abs(logTest - predTest)
         acc_sum += acc
         if print_result=='y':
@@ -282,10 +285,41 @@ for x in range(time):
     nb_acc = acc_sum / total
     nb_avg_sum += nb_acc
     print('\nnaïve Bayes predict diff = %f' % nb_acc)
-
     print()
 
+    # process line
+    # print('time = %d' % time)
+    # print('x = %d' % x)
+
+    i_per = math.floor(((x+1)/time)*100)
+    k = i_per + 1
+    pl = '['+ '#'*(i_per//2)+' '*((100-k)//2) + ']'
+    sys.stderr.write('\r'+pl+' (%s%%)'%(i_per))
+    sys.stderr.flush()
+    # i_print = math.floor((i/time)*50)
+    # sys.stderr.write('\r')
+    # sys.stderr.write("[%-50s] %.2f%%" % ('='*i_print, i_per))
+    # sys.stderr.flush()
+
+	# end = time.time()
+	# elapsed = end - start
+	# elapsed = elapsed / 60
+
+
+
+	# sys.stdout.write('Time taken: ')
+	# sys.stdout.write("%d" % elapsed)
+	# sys.stdout.write(' min.\t')
+
+
+
 # avg test result
+sys.stderr.write('\n\n\n===== test results =====')
+sys.stderr.write('\n\ndecision tree avg diff = %f' % (tree_avg_sum / time))
+sys.stderr.write('\n\nkNN avg diff = %f' % (knn_avg_sum / time))
+sys.stderr.write('\n\nnaïve Bayes avg diff = %f' % (nb_avg_sum / time))
+sys.stderr.write('\n\n')
+
 print('\n\n===== test results =====')
 print('\ndecision tree avg diff = %f' % (tree_avg_sum / time))
 print('\nkNN avg diff = %f' % (knn_avg_sum / time))
