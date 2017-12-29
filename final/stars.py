7b9ed904223ae049b3ebc1eba212a8043d5a971c
cysun0226 <j6256448@gmail.com>
2017-12-29 20:41:59 +0800

    Finish reading file

--- final/stars.py
+++ final/stars.py
@@ -25,29 +25,36 @@ nb_avg_sum = 0
 # read star.csv
 with open(star_file, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    i = 0
     for row in spamreader:
-        row = row[0].split(',')
-        lines.append(row)
+        if i == 0: # header
+            row = row[0].split(',')
+            lines.append(row)
+            i += 1
+        else:
+            row = row[2].split(',')
+            lines.append(row)
+
 feature_names = lines[0]
 
-print(feature_names)
-del feature_names[0] # remove time stamp
-print(feature_names)
-del feature_names[len(feature_names)-2] # remove commands
-print(feature_names)
 
 # print result header
+del feature_names[0] # remove time stamp
+del feature_names[len(feature_names)-2] # remove commands
+print(feature_names)
 print('\n\n===== star-sign data set =====\n')
-print('資料總數 = %d 份' % len(lines))
+print('資料總數 = %d 份\n' % len(lines))
+total_num = len(lines)
 # print('train_set = %d' % math.floor(0.9*len(lines)))
 # print('test_set = %d' % (len(lines) - math.floor(0.1*len(lines))))
-#
+
 # sys.stderr.write("\nexecute time: ")
 # time = int(input("\nexecute time: "))
 # sys.stderr.write("\nprint detailed results?(y/n): ")
 # print_result = input("print detailed results?(y/n): ")
 # sys.stderr.write("\n\n--- process status ---\n\n")
-#
+
+
 # for x in range(time):
 #     print('\n\n===== test %d =====' % (x+1))
 #
@@ -59,42 +66,103 @@ print('資料總數 = %d 份' % len(lines))
 #     day_list = []
 #     month_list = []
 #
-#     # train set
-#     for line in train_set:
-#         d = []
-#         cd = []
-#         d.append(int(line[0])) # X
-#         d.append(int(line[1])) # Y
-#         d.append(month_to_int(line[2])) # month
-#         d.append(day_to_int(line[3])) # day
-#         d.append(float(line[4])) # FFMC
-#         d.append(float(line[5])) # DMC
-#         d.append(float(line[6])) # DC
-#         d.append(float(line[7])) # ISI
-#         d.append(float(line[8])) # temp
-#         d.append(float(line[9])) # RH
-#         d.append(float(line[10])) # wind
-#         d.append(float(line[11])) # rain
-#         data.append(d)
-#         db = list(d)
-#         del db[2]
-#         del db[2]
-#         cont_data.append(db)
-#         cd.append(month_to_int(line[2]))
-#         cd.append(day_to_int(line[3]))
-#         cat_data.append(cd)
-#         target.append(float(line[12]))
-#         # if float(line[12]) == 0:
-#         #     target.append(-10) # area
-#         # else:
-#         #     target.append(math.log10(float(line[12]))) # area
-#
-#     forestfire['target'] = array(target)
-#     forestfire['feature_names'] = array(feature_names)
-#     forestfire['data'] = array(data)
-#     forestfire_nb['target'] = array(target)
-#     forestfire_nb['cont_data'] = array(cont_data)
-#     forestfire_nb['cat_data'] = array(cat_data)
+
+# lines
+del lines[0]
+i = 0
+for line in lines:
+    del line[0] # remove time
+    del line[len(line)-2]
+
+i = 0
+for line in lines:
+    if len(line) != 33:
+        # print('line[%d] has incorrect element num' % i)
+        total_num -= 1
+        continue
+    d = {}
+    d['分析對象'] = line[0]
+    d['星座'] = line[1]
+    d['耐性'] = int(line[2])
+    d['脾氣暴躁'] = int(line[3])
+    d['幼稚'] = int(line[4])
+    d['頑固'] = int(line[5])
+    d['心思細膩'] = int(line[6])
+    d['保守'] = int(line[7])
+    d['冷靜'] = int(line[8])
+    d['樂觀'] = int(line[9])
+    d['活潑'] = int(line[10])
+    d['公正'] = int(line[11])
+    d['優柔寡斷'] = int(line[12])
+    d['強勢'] = int(line[13])
+    d['浪漫'] = int(line[14])
+    d['過度理想化'] = int(line[15])
+    d['斤斤計較'] = int(line[16])
+    d['心機重'] = int(line[17])
+    d['完美主義'] = int(line[18])
+    d['愛計仇'] = int(line[19])
+    d['與眾不同'] = int(line[20])
+    d['愛面子'] = int(line[21])
+    d['有魅力'] = int(line[22])
+    d['正義感'] = int(line[23])
+    d['重視友情'] = int(line[24])
+    d['專情'] = int(line[25])
+    d['愛哭'] = int(line[26])
+    d['顧家'] = int(line[27])
+    d['體貼'] = int(line[28])
+    d['情緒化'] = int(line[29])
+    d['口才'] = int(line[30])
+    d['創意'] = int(line[31])
+    d['潔癖'] = int(line[32])
+    data.append(d)
+    i += 1
+
+# Aries
+Aries = []
+for row in data:
+    if(row['星座'] == '牡羊座'):
+        Aries.append(row)
+
+# 
+
+
+# for d in data:
+#     print(d)
+
+    # d = []
+    # cd = []
+    # d.append(int(line[0])) # X
+    # d.append(int(line[1])) # Y
+    # d.append(month_to_int(line[2])) # month
+    # d.append(day_to_int(line[3])) # day
+    # d.append(float(line[4])) # FFMC
+    # d.append(float(line[5])) # DMC
+    # d.append(float(line[6])) # DC
+    # d.append(float(line[7])) # ISI
+    # d.append(float(line[8])) # temp
+    # d.append(float(line[9])) # RH
+    # d.append(float(line[10])) # wind
+    # d.append(float(line[11])) # rain
+    # data.append(d)
+    # db = list(d)
+    # del db[2]
+    # del db[2]
+    # cont_data.append(db)
+    # cd.append(month_to_int(line[2]))
+    # cd.append(day_to_int(line[3]))
+    # cat_data.append(cd)
+    # target.append(float(line[12]))
+    # if float(line[12]) == 0:
+    #     target.append(-10) # area
+    # else:
+    #     target.append(math.log10(float(line[12]))) # area
+
+# forestfire['target'] = array(target)
+# forestfire['feature_names'] = array(feature_names)
+# forestfire['data'] = array(data)
+# forestfire_nb['target'] = array(target)
+# forestfire_nb['cont_data'] = array(cont_data)
+# forestfire_nb['cat_data'] = array(cat_data)
 #
 #     # test set
 #     test_data = []
